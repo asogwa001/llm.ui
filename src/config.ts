73e34f6505f1ee908d080f8f48f0e0e775862337
Asogwa Emmanuel <easogwa@gitam.in>
2025-09-08 23:04:54 +0100

    fill in and lock edu.ai endpoint details

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export const CONFIG_DEFAULT: Readonly<Configuration> = Object.freeze({
   // Note: in order not to introduce breaking changes, please keep the same data type (number, string, etc) if you want to change the default value. Do not use null or undefined for default value.
   // Do not use nested objects, keep it single level. Prefix the key if you need to group them.
   provider: 'edu-ai',
-  baseUrl: 'https://api.edu.ai/v1',
+  baseUrl: 'http://34.250.146.45',
   apiKey: '<API_KEY>',
   model: 'akili-tool',
   systemMessage: '',
@@ -166,7 +166,7 @@ export const SYNTAX_THEMES = Object.freeze([
 // list of inference providers
 export const INFERENCE_PROVIDERS: Readonly<InferenceProviders> = Object.freeze({
   'edu-ai': {
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://34.250.146.45',
     name: 'EDU.ai',
     icon: 'assets/providers/llamacpp.svg',
     allowCustomBaseUrl: false,
